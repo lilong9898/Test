8161bb7d2815a645bf7c5f3000da56648ac7f65f
lilong <lilong@zhangyue.com>
2018-08-15 19:49:48 +0800

    more

---
+++ FragmentTest/src/main/java/com/lilong/fragmenttest/activity/MainActivity.java
@@ -0,0 +1,45 @@
+package com.lilong.fragmenttest.activity;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+import com.lilong.fragmenttest.R;
+import com.lilong.fragmenttest.base.BaseActivity;
+import com.lilong.fragmenttest.fragment.SecondFragment;
+
+/**
+ * Created by lilong on 18-7-18.
+ */
+
+public class MainActivity extends BaseActivity {
+
+    private Button btnJumpToSecondActivity;
+    private SecondFragment secondFragment;
+
+    @Override
+    public int getLayoutResourceId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initView() {
+        btnJumpToSecondActivity = (Button) findViewById(R.id.btnJumpToSecondActivity);
+        btnJumpToSecondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+        secondFragment = new SecondFragment();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (getFragmentManager().findFragmentById(R.id.containerSecondFragment) == null) {
+            getFragmentManager().beginTransaction().add(R.id.containerSecondFragment, secondFragment).commitAllowingStateLoss();
+        }
+    }
+}
