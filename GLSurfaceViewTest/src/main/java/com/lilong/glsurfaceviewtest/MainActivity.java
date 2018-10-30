e91e54108d1ad008ece897a65b427cd34bc579e7
lilong <lilong@zhangyue.com>
2018-10-30 19:24:06 +0800

    more

---
+++ GLSurfaceViewTest/src/main/java/com/lilong/glsurfaceviewtest/MainActivity.java
@@ -0,0 +1,23 @@
+package com.lilong.glsurfaceviewtest;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    private GLSurfaceView glSurfaceView;
+    private GLSurfaceView.Renderer glSurfaceRender;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        glSurfaceView = findViewById(R.id.glSurfaceView);
+        glSurfaceRender = new MainRender(getResources());
+        glSurfaceView.setRenderer(glSurfaceRender);
+        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+    }
+
+
+}
