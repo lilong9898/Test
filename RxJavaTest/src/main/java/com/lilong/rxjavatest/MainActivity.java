557b33a80632cefb33eff4e2a57f9d32771ca3d8
lilong <lilong@zhangyue.com>
2018-10-30 21:35:10 +0800

    more

---
+++ RxJavaTest/src/main/java/com/lilong/rxjavatest/MainActivity.java
@@ -0,0 +1,84 @@
+package com.lilong.rxjavatest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.Arrays;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends Activity {
+
+    private static final String TAG = "RxJava";
+
+    // 观察者
+    private Observer<String> observer = new Observer<String>() {
+        @Override
+        public void onSubscribe(Disposable d) {
+            Log.i(TAG, "onSubscribe " + d);
+        }
+
+        @Override
+        public void onNext(String s) {
+            Log.i(TAG, "onNext " + s);
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            Log.i(TAG, "onError " + Log.getStackTraceString(e));
+        }
+
+        @Override
+        public void onComplete() {
+            Log.i(TAG, "onComplete");
+        }
+    };
+
+    // 另一种形式的观察者
+    private Subscriber<String> subscriber = new Subscriber<String>() {
+        @Override
+        public void onSubscribe(Subscription s) {
+            Log.i(TAG, "onSubscribe " + s);
+        }
+
+        @Override
+        public void onNext(String s) {
+            Log.i(TAG, "onNext " + s);
+        }
+
+        @Override
+        public void onError(Throwable t) {
+            Log.i(TAG, "onError " + Log.getStackTraceString(t));
+        }
+
+        @Override
+        public void onComplete() {
+            Log.i(TAG, "onComplete");
+        }
+    };
+
+    // 被观察者的数据源
+    private String[] sources = new String[]{"a", "b"};
+
+    // 被观察者
+    private Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
+        @Override
+        public void subscribe(ObservableEmitter<String> e) throws Exception {
+//            Arrays.asList(sources).forEach(
+        }
+    });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
