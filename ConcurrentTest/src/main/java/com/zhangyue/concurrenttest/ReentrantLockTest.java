da6e345a2488fa2dd7630d940d6090dd8065ab90
lilong <lilong@zhangyue.com>
2019-01-17 21:30:18 +0800

    more

--- ConcurrentTest/src/main/java/com/zhangyue/concurrenttest/ReentrantLockTest.java
+++ ConcurrentTest/src/main/java/com/zhangyue/concurrenttest/ReentrantLockTest.java
@@ -1,40 +1,31 @@
 package com.zhangyue.concurrenttest;
 
-/**
- * synchronized关键字效果：
- * <p>
- * public synchronized void method(){
- * xxxxxxxxx
- * }
- * 等效于
- * public void method(){
- * synchronized(this){
- * <p>
- * }
- * }
- * 所以当一个线程在访问synchronized方法时，另一线程不能访问这个方法，或其它synchronized方法
- * 因为正在访问synchronized方法的线程获取了[当前对象]这个锁，其它线程要访问这个方法或其他synchronized方法都需要获得这个锁
- */
-public class SynchronizedKeyWordTest {
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockTest {
 
     public static void main(String[] args) {
-
         Data data = new Data();
 
         IncreaseThread increaseThread1 = new IncreaseThread("1", data);
         IncreaseThread increaseThread2 = new IncreaseThread("2", data);
-        DecreaseThread decreaseThread1 = new DecreaseThread("1", data);
 
         increaseThread1.start();
         increaseThread2.start();
-        decreaseThread1.start();
     }
 
     static class Data {
 
+        private ReentrantLock lock;
+
+        public Data() {
+            lock = new ReentrantLock();
+        }
+
         public int number = 0;
 
-        public synchronized void increase(String name) {
+        public void increase(String name) {
+            lock.lock();
             for (int i = 0; i < 5; i++) {
                 number = number + 1;
                 System.out.println("IncreaseThread " + name + ", number = " + number);
@@ -43,9 +34,10 @@ public class SynchronizedKeyWordTest {
                 } catch (Exception e) {
                 }
             }
+            lock.unlock();
         }
 
-        public synchronized void decrease(String name) {
+        public void decrease(String name) {
             for (int i = 0; i < 5; i++) {
                 number = number - 1;
                 System.out.println("DecreaseThread" + name + ", number = " + number);
@@ -82,9 +74,9 @@ public class SynchronizedKeyWordTest {
     static class DecreaseThread extends Thread {
 
         private String name;
-        private Data data;
+        private SynchronizedKeyWordTest.Data data;
 
-        public DecreaseThread(String name, Data data) {
+        public DecreaseThread(String name, SynchronizedKeyWordTest.Data data) {
             this.name = name;
             this.data = data;
         }
@@ -96,3 +88,5 @@ public class SynchronizedKeyWordTest {
     }
 
 }
+
+
