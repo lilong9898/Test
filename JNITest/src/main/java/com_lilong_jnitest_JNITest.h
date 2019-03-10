8ec369b11379c4d5e7462d13cd76d075746555b7
lilong <lilong9898@gmail.com>
2019-03-10 14:57:04 +0800

    more

--- JNITest/src/main/java/com_lilong_jnitest_JNITest.h
+++ JNITest/src/main/java/com_lilong_jnitest_JNITest.h
@@ -8,6 +8,7 @@
 
 /**
 本头文件需引入jni库的头文件jni.h
+这个文件的实际位置是 jdk目录/include/jni.h
 */
 #include <jni.h>
 /**
@@ -16,7 +17,8 @@
        但是有时希望对其中一部分内容只在满足一定条件才进行编译
        也就是对一部分内容指定编译的条件，这就是“条件编译”
 
-下面带#号的都是条件编译语句， 为防止不同的引入该头文件的C文件都执行
+下面带#号的都是预处理语句， 而涉及#ifdef/#ifndef的是条件编译语句
+为防止不同的引入该头文件的C文件都执行
 
 JNIEXPORT jint JNICALL Java_com_lilong_jnitest_JNITest_add
   (JNIEnv *, jclass, jint, jint);
@@ -40,8 +42,24 @@ extern "C" {
  * Class:     com_lilong_jnitest_JNITest
  * Method:    add
  * Signature: (II)I
+
+ JNIEXPORT
+          是jni.h中定义的宏: #define JNIEXPORT  __attribute__ ((visibility ("default")))
+          给编译器看的，可选
+          (1) 如果编译so时使用了-fvisibility选项
+              它可以让这个函数出现在so库的符号表中，使得java可以调用
+              否则不能调用
+          (2) 如果编译so时没用-fvisibility选项
+              则所有方法都会出现在so库的符号表中，无论是否使用JNIEXPORT
+
+ JNICALL
+          是jni.h中定义的宏： #define JNICALL
+          给编译器看的，必备
+          在不同平台上的jni.h文件中，此宏被定义成不同样子，让编译器正确编译所必须
+
+ JNIEnv *是个指向JNI环境的指针，可通过它来访问JNI提供的接口方法
+ jclass 实际上是jobject *，是个指向不定类型对象的指针，是java中的this
  */
- // JNIEXPORT和JNICALL都是jni.h中定义的宏
 JNIEXPORT jint JNICALL Java_com_lilong_jnitest_JNITest_add
   (JNIEnv *, jclass, jint, jint);
 
