08e23c157cc1c5cc2052d451b94991a3207e48dc
lilong <lilong@zhangyue.com>
2019-03-13 21:30:57 +0800

    more

---
+++ ViewPositionTest/src/main/java/com/lilong/viewpositiontest/CustomViewGroup.java
@@ -0,0 +1,97 @@
+package com.lilong.viewpositiontest;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import static com.lilong.viewpositiontest.MainActivity.TAG;
+
+public class CustomViewGroup extends RelativeLayout {
+
+    private Context context;
+    private Paint paint;
+    private String name;
+
+    /**
+     * 是否在{@link #onDraw(Canvas)}中平移canvas
+     */
+    private boolean translateCanvas = false;
+
+    /**
+     * 子View是否已经添加过了
+     * */
+    private boolean childViewAddedAlready = false;
+
+    public CustomViewGroup(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public CustomViewGroup(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    private void init(final Context context) {
+        this.context = context;
+        name = getResources().getResourceEntryName(getId());
+        setBackgroundColor(getResources().getColor(android.R.color.holo_orange_dark));
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setTextSize(80);
+        paint.setColor(Color.WHITE);
+        paint.setStrokeWidth(10);
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, name + "is clicked");
+            }
+        });
+    }
+
+    public void setTranslateCanvas(boolean translateCanvas) {
+        this.translateCanvas = translateCanvas;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        if(!childViewAddedAlready){
+            View v = new View(context);
+            RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(getMeasuredWidth() / 3, getMeasuredHeight() / 3);
+            lp.addRule(CENTER_IN_PARENT);
+            v.setLayoutParams(lp);
+            v.setBackgroundColor(0x5000ff00);
+            addView(v);
+            childViewAddedAlready = true;
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int width = getWidth();
+        int height = getHeight();
+        float textWidth = paint.measureText(name);
+        canvas.save();
+        if(translateCanvas){
+            canvas.translate(width/3, 0);
+        }
+        canvas.drawText(name, width / 2 - textWidth / 2, height / 2, paint);
+        canvas.restore();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float rawX = event.getRawX();
+        Log.i(TAG, name + ": x = " + x + ", rawX = " + rawX + ", left = " + getLeft() + ", x = "  +getX());
+        return true;
+    }
+}
---
+++ ViewPositionTest/src/main/java/com/lilong/viewpositiontest/MainActivity.java
@@ -0,0 +1,59 @@
+package com.lilong.viewpositiontest;
+
+import android.app.Activity;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * (1)
+ * {@link MotionEvent#getX()}返回的是事件相对于控件自身的位置
+ * {@link MotionEvent#getRawX()}返回的是事件相对于屏幕边缘的位置
+ *
+ * (2)
+ * {@link View#setTranslationX(float)}会改变控件的事件响应区域, 使得它与控件的可视区域一致
+ * 但是不改变{@link View#getLeft()}的值
+ *
+ * 官方解释:
+ * android:translationX
+ * translation in x of the view. This value is added post-layout to the left property of the view, which is set by its layout.
+ *
+ * 这应该是在layout过程后, translationX被加到left/right属性上
+ *
+ * (3)
+ * {@link Canvas#translate(float, float)}只改变绘制, 不改变事件相应区域
+ *
+ * */
+public class MainActivity extends Activity {
+
+    public static final String TAG = "VTest";
+
+    private CustomViewGroup v1;
+    private CustomViewGroup v2;
+    private CustomViewGroup v3;
+    private CustomViewGroup v4;
+
+    private boolean alreadyTest = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        v1 = findViewById(R.id.v1);
+        v2 = findViewById(R.id.v2);
+        v3 = findViewById(R.id.v3);
+        v4 = findViewById(R.id.v4);
+        v3.setTranslateCanvas(true);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus && !alreadyTest){
+            v2.setTranslationX(v2.getWidth());
+            v4.scrollBy(v4.getWidth() / 3, 0);
+            alreadyTest = true;
+        }
+    }
+}
