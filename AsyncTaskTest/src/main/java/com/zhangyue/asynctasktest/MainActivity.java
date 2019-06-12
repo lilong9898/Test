5dac3aee94b7e53f51486ba0f6078634f8b86daa
lilong <lilong@zhangyue.com>
2019-06-12 10:44:49 +0800

    more

---
+++ AsyncTaskTest/src/main/java/com/zhangyue/asynctasktest/MainActivity.java
@@ -0,0 +1,76 @@
+package com.zhangyue.asynctasktest;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * {@link AsyncTask}在
+ * (1) android 4之前用{@link AsyncTask#SERIAL_EXECUTOR}
+ * (2) android 5-11用{@link AsyncTask#THREAD_POOL_EXECUTOR}
+ * (3) android 12以后用{@link AsyncTask#SERIAL_EXECUTOR}
+ *
+ * 注意，
+ * (1) SERIAL_EXECUTOR是AsyncTask类的静态成员，所以同一个进程内的多个AsyncTask也是串行执行的
+ * (2) SERIAL_EXECUTOR内部有队列用来存储提交的任务，一个任务执行完，才会从队列中取出下一个任务
+ * (3) SERIAL_EXECUTOR起到的是串行化的作用，真正执行内部futureTask的还是THREAD_POOL_EXECUTOR
+ * (4) {@link AsyncTask#onPreExecute()}会在调用{@link AsyncTask#execute(Object[])}后立即执行，不管是否是串行执行
+ */
+public class MainActivity extends Activity {
+
+    private static final String TAG = "ATest";
+
+    private Button btnStartAsyncTask;
+    private TextView tvResult;
+    private CustomAsyncTask asyncTask;
+    private int taskNumber = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnStartAsyncTask = findViewById(R.id.btnStartAsyncTask);
+        tvResult = findViewById(R.id.tvResult);
+        btnStartAsyncTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvResult.setText("pending...");
+                asyncTask = new CustomAsyncTask(taskNumber++);
+                asyncTask.execute();
+            }
+        });
+    }
+
+    class CustomAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        private int taskNumber;
+
+        public CustomAsyncTask(int taskNumber){
+            this.taskNumber = taskNumber;
+        }
+
+        @Override
+        protected void onPreExecute() {
+//            Log.i(TAG, "task " + taskNumber + " onPreExecute on thread " + Thread.currentThread().getName());
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Log.i(TAG, "task " + taskNumber + " doInBackground on thread group = " + Thread.currentThread().getThreadGroup().getName() + ", name = " + Thread.currentThread().getName());
+            try{
+                Thread.sleep(2000);
+            }catch (Exception e){}
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Log.i(TAG, "task " + taskNumber + " onPostExecute on thread " + Thread.currentThread().getName());
+            Log.i(TAG, "-----------------------------------------");
+        }
+    }
+}
