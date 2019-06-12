8b551140ff0ef0fdc99d1421a0ba329b2c5f8516
lilong <lilong@zhangyue.com>
2019-06-12 20:58:01 +0800

    more

--- DesignPattern/src/main/java/com/lilong/designpattern/PatternSingleton.java
+++ DesignPattern/src/main/java/com/lilong/designpattern/PatternSingleton.java
@@ -2,25 +2,63 @@ package com.lilong.designpattern;
 
 /**
  * 单例模式
- * 使用DoubleCheckLock模式,节省不必要的上锁,用volatile关键字避免多线程下各个线程的寄存器与内存不一致的问题
+ * 先加载和后加载两种方式
  */
 
 public class PatternSingleton {
 
     public static void main(String[] args) {
-        System.out.println("get singleton : " + getInstance());
+        System.out.println("get hungry singleton : " + HungrySingleton.getInstance());
+        System.out.println("get hungry singleton : " + HungrySingleton.getInstance());
+        System.out.println("get lazy singleton : " + LazySingleton.getInstance());
+        System.out.println("get lazy singleton : " + LazySingleton.getInstance());
     }
 
-    private static volatile PatternSingleton mSingleton;
+}
 
-    private PatternSingleton() {
+/**
+ * 先加载方式
+ * 利用类只会加载一次的特性，在静态变量初始化时（方法１）或静态块中（方法２）中构造实例，使得这个构造也只有一次
+ * */
+class HungrySingleton {
+
+    //-------------方法1-----------------------
+//    private static HungrySingleton mSingleton = new HungrySingleton();
+
+    //-------------方法2-----------------------
+    private static HungrySingleton mSingleton;
+
+    static {
+        mSingleton = new HungrySingleton();
+    }
+    //-----------------------------------------
+
+    private HungrySingleton(){
+
     }
 
-    public static PatternSingleton getInstance() {
+    public static HungrySingleton getInstance(){
+        return mSingleton;
+    }
+
+}
+
+/**
+ * 后加载方式
+ * 使用DoubleCheckLock模式,节省不必要的上锁,用volatile关键字避免多线程下各个线程的寄存器与内存不一致的问题
+ * */
+class LazySingleton {
+
+    private static volatile LazySingleton mSingleton;
+
+    private LazySingleton() {
+    }
+
+    public static LazySingleton getInstance() {
         if (mSingleton == null) {
-            synchronized (PatternSingleton.class) {
+            synchronized (LazySingleton.class) {
                 if (mSingleton == null) {
-                    mSingleton = new PatternSingleton();
+                    mSingleton = new LazySingleton();
                 }
             }
         }
