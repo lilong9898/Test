eace14b7330b1890a5ff57788e65d277953780a1
lilong <lilong@zhangyue.com>
2019-07-08 17:52:15 +0800

    more

--- RemoteViewTest/src/main/java/com/lilong/remoteviewtest/MainActivity.java
+++ RemoteViewTest/src/main/java/com/lilong/remoteviewtest/MainActivity.java
@@ -3,6 +3,7 @@ package com.lilong.remoteviewtest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +16,7 @@ public class MainActivity extends Activity {
 
     private EditText edt;
     private Button btnSet;
+    private Button btnStartSecond;
     private RemoteViews rv;
     private String content = "default";
 
@@ -24,17 +26,23 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         edt = findViewById(R.id.edt);
         btnSet = findViewById(R.id.btnSet);
-        rv = new RemoteViews(getPackageName(), R.layout.layout_remote);
-        Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra(KEY_REMOTE_VIEWS, rv);
-        startActivity(intent);
         btnSet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 content = edt.getText().toString();
-                rv.setTextViewText(R.id.remoteTv, content);
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 intent.putExtra(KEY_REMOTE_VIEWS, rv);
+                startActivity(intent);
+            }
+        });
+        btnStartSecond = findViewById(R.id.btnStartSecond);
+        btnStartSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rv = new RemoteViews(getPackageName(), R.layout.layout_remote);
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra(KEY_REMOTE_VIEWS, rv);
+                Log.i(TAG, "MainActivity sends RemoteViews " + rv);
                 startActivity(intent);
             }
         });
--- RemoteViewTest/src/main/java/com/lilong/remoteviewtest/SecondActivity.java
+++ RemoteViewTest/src/main/java/com/lilong/remoteviewtest/SecondActivity.java
@@ -1,10 +1,17 @@
 package com.lilong.remoteviewtest;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.RemoteViews;
+
+import static com.lilong.remoteviewtest.MainActivity.TAG;
 
 public class SecondActivity extends Activity {
 
@@ -16,7 +23,18 @@ public class SecondActivity extends Activity {
         setContentView(R.layout.activity_second);
         layoutContainer = findViewById(R.id.layoutContainer);
         RemoteViews remoteViews = getIntent().getParcelableExtra(MainActivity.KEY_REMOTE_VIEWS);
-        remoteViews.apply(this, layoutContainer);
+        Log.i(TAG, "SecondActivity gets RemoteViews " + remoteViews);
+
+        /**
+         * {@link RemoteViews#apply(Context, ViewGroup)}内部使用了{@link LayoutInflater#inflate(int, ViewGroup, boolean)}方法，attachParent是false
+         * 所以这里的layoutContainer的作用仅仅是帮助生成layout_remote的layoutParams而已
+         * appliedView本身没有parent，需要手动加到合适的parent上
+         * */
+        View appliedView = remoteViews.apply(this, layoutContainer);
+        Log.i(TAG, "SecondActivity RemoteViews applies and gets " + appliedView + " with tag of " + appliedView.getTag() + " and parent of " + appliedView.getParent());
+
+        /** 加到parent上才能显示出来*/
+        layoutContainer.addView(appliedView);
     }
 
     @Override
