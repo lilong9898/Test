03025e713a19e244f54e1d8ffac15c774ff4c4f8
lilong <lilong@zhangyue.com>
2019-07-08 19:08:58 +0800

    more

--- RemoteViewTest/src/main/java/com/lilong/remoteviewtest/MainActivity.java
+++ RemoteViewTest/src/main/java/com/lilong/remoteviewtest/MainActivity.java
@@ -1,14 +1,25 @@
 package com.lilong.remoteviewtest;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RemoteViews;
 
+/**
+ * 使用{@link RemoteViews}来跨进程传递UI，假设从A进程传递给B进程
+ * (1) RemoteViews本身不是View，是View的信息，因为其实现{@link Parcelable}接口，所以可以放入{@link Intent}里以及跨进程传递
+ * (2) 在B进程里，必须调{@link RemoteViews#apply(Context, ViewGroup)}根据RemoteViews里的信息创建出View，然后将这个View加到view tree里
+ * (3) {@link RemoteViews#setTextViewText(int, CharSequence)}这类方法[只是记录了一些操作]，必须在B进程里调{@link RemoteViews#reapply(Context, View)}才能将这些操作生效
+ *
+ * 所以本质上RemoteViews只能用来传递构建UI所需的信息，构建动作/刷新动作都在B进程里
+ * */
 public class MainActivity extends Activity {
 
     public static final String TAG = "RTest";
@@ -29,7 +40,12 @@ public class MainActivity extends Activity {
         btnSet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /**
+                 * 在主进程中重建一个RemoteViews，传给另一个进程中的SecondActivity，让其更新上这个新的RemoteViews
+                 * */
+                rv = new RemoteViews(getPackageName(), R.layout.layout_remote);
                 content = edt.getText().toString();
+                rv.setTextViewText(R.id.tvRemote, "RemoteView in another process" + " : " + content);
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 intent.putExtra(KEY_REMOTE_VIEWS, rv);
                 startActivity(intent);
--- RemoteViewTest/src/main/java/com/lilong/remoteviewtest/SecondActivity.java
+++ RemoteViewTest/src/main/java/com/lilong/remoteviewtest/SecondActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.RelativeLayout;
 import android.widget.RemoteViews;
 
@@ -16,13 +17,23 @@ import static com.lilong.remoteviewtest.MainActivity.TAG;
 public class SecondActivity extends Activity {
 
     private RelativeLayout layoutContainer;
+    private View appliedView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         layoutContainer = findViewById(R.id.layoutContainer);
+        // 让透明的SecondActivity不要消费触摸事件，事件传给后面的MainActivity
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH |
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN |
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
         RemoteViews remoteViews = getIntent().getParcelableExtra(MainActivity.KEY_REMOTE_VIEWS);
+        if(remoteViews == null){
+            return;
+        }
         Log.i(TAG, "SecondActivity gets RemoteViews " + remoteViews);
 
         /**
@@ -30,7 +41,7 @@ public class SecondActivity extends Activity {
          * 所以这里的layoutContainer的作用仅仅是帮助生成layout_remote的layoutParams而已
          * appliedView本身没有parent，需要手动加到合适的parent上
          * */
-        View appliedView = remoteViews.apply(this, layoutContainer);
+        appliedView = remoteViews.apply(this, layoutContainer);
         Log.i(TAG, "SecondActivity RemoteViews applies and gets " + appliedView + " with tag of " + appliedView.getTag() + " and parent of " + appliedView.getParent());
 
         /** 加到parent上才能显示出来*/
@@ -40,8 +51,9 @@ public class SecondActivity extends Activity {
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-        layoutContainer.removeAllViews();
         RemoteViews remoteViews = intent.getParcelableExtra(MainActivity.KEY_REMOTE_VIEWS);
-        remoteViews.apply(this, layoutContainer);
+        if(appliedView != null && remoteViews != null){
+            remoteViews.reapply(this, appliedView);
+        }
     }
 }
