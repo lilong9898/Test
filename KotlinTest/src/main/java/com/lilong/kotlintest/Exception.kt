05c4d16b7f45b54354d29833f501b68255b91bb6
lilong01 <lilong01@fenbi.com>
2019-09-18 12:30:24 +0800

    more

---
+++ KotlinTest/src/main/java/com/lilong/kotlintest/Exception.kt
@@ -0,0 +1,21 @@
+package com.lilong.kotlintest
+
+fun main(){
+    callMayThrow()
+}
+
+fun callMayThrow(){
+    mayThrow(2)
+}
+
+/**
+ * kotlin中没有checkedException
+ * 调用一个可能抛出异常的方法，不写try catch也不会被编译器报错
+ * [@throws]注解不影响这一点，仅影响字节码层面是否给这个方法的签名加throws标记
+ * */
+@Throws(RuntimeException::class)
+fun mayThrow(arg: Int){
+    if(arg > 1){
+        throw RuntimeException()
+    }
+}
