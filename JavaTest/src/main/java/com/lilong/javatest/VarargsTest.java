26ef853ce914b138df7fbe5f8d6b78d8d7640fe5
lilong01 <lilong01@fenbi.com>
2020-01-02 12:22:15 +0800

    more

---
+++ JavaTest/src/main/java/com/lilong/javatest/VarargsTest.java
@@ -0,0 +1,18 @@
+package com.lilong.javatest;
+
+/**
+ * Created by lilong on 02/01/2020.
+ */
+public class VarargsTest {
+
+    public static void main(String[] args){
+
+    }
+
+    private static void fun1(Object... varargs){
+    }
+
+    private static void fun2(){
+
+    }
+}
--- KotlinTest/src/main/java/com/lilong/kotlintest/coroutine/SpreadOperator.kt
+++ KotlinTest/src/main/java/com/lilong/kotlintest/coroutine/SpreadOperator.kt
@@ -6,15 +6,15 @@ package com.lilong.kotlintest.coroutine
 
 fun main(){
     val array = intArrayOf(1, 2, 3)
-    funWithVarargs(*array)
+    fun1(*array)
     /**
      *  上面这句，在字节码层面是
-     *  funWithVarargs(Arrays.copyOf(array, array.length));
+     *  fun1(Arrays.copyOf(array, array.length));
      *  spread operator "*" 会导致一次 array copy，所以要避免使用
      * */
 }
 
-fun funWithVarargs(vararg array: Int){
+fun fun1(vararg array: Int){
 
 }
 
