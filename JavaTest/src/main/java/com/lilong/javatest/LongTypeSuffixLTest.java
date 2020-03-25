f83abb910b46167b9864d28be510c0c12c510362
lilong01 <lilong01@fenbi.com>
2020-03-25 14:03:17 +0800

    more

---
+++ JavaTest/src/main/java/com/lilong/javatest/LongTypeSuffixLTest.java
@@ -0,0 +1,11 @@
+package com.lilong.javatest;
+
+/**
+ * Created by lilong on 23/03/2020.
+ */
+public class LongTypeSuffixLTest {
+
+    public static void main(String[] args){
+        long variable = 1;
+    }
+}
